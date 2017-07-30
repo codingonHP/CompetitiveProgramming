8de4d28b0a4a3597f85c7d583b30887534c64ee5
Vishal Anand <vishal_24_anand@hotmail.com>
2017-07-30 16:24:53 +0530

    Sparse Table

---
+++ CompetitiveProgramming/LC_Shortest_Unsorted_Continuous_Subarray.cs
@@ -0,0 +1,99 @@
+﻿//Solution to the problem belongs to me
+using System;
+using System.Linq;
+
+namespace CpForCompetitiveProgrammingLcShortestUnsortedContinuousSubarray
+{
+    public static class LcShortestUnsortedContinuousSubarray
+    {
+        #region Main
+
+        public static void Main_Solver(string[] args)
+        //public static void Main(string[] args)
+        {
+            var array = Array.ConvertAll(Console.ReadLine().Split(' ').ToArray(), int.Parse);
+            var len = Solve(array);
+
+            Console.WriteLine(len);
+        }
+
+        #endregion
+
+        #region Solution
+
+        public static int Solve(int[] array)
+        {
+            int prev = array[0];
+            int minIndex = 0;
+            bool inc = true;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (prev > array[i])
+                {
+                    inc = false;
+                    break;
+                }
+
+                minIndex = i;
+                prev = array[i];
+            }
+
+            if (inc)
+            {
+                return 0;
+            }
+
+            var next = array[array.Length - 1];
+            int maxIndex = array.Length - 1;
+
+            for (int i = array.Length - 2; i >= 0; i--)
+            {
+               
+                if (next < array[i])
+                {
+                    break;
+                }
+
+                maxIndex = i;
+                next = array[i];
+
+            }
+
+            var minValue = (int)1e9;
+            var maxValue = (int)-1e9;
+
+            for (int i = minIndex; i <= maxIndex; i++)
+            {
+                if (array[i] > maxValue)
+                {
+                    maxValue = array[i];
+                }
+
+                if (array[i] < minValue)
+                {
+                    minValue = array[i];
+                }
+            }
+
+            int dl = 0, dr = 0;
+            int l = minIndex - 1, r = maxIndex + 1;
+
+            while (l >= 0 && array[l] > minValue)
+            {
+                ++dl;
+                --l;
+            }
+
+            while (r < array.Length && array[r] < maxValue)
+            {
+                ++dr;
+                ++r;
+            }
+
+            return maxIndex - minIndex + 1 + dl + dr;
+        }
+
+        #endregion
+    }
+}
---
+++ UnitTest/SparseTableTest.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using CpForCompetitiveProgrammingSparseTable;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestSparseTableTest
+{
+    class Settings
+    {
+        public bool ShowAll { get; set; }
+        public bool OnlyFailed { get; set; }
+        public bool OnlyPassed { get; set; }
+        public long IterationCount { get; set; }
+        public long SubIterationCount { get; set; }
+        public int MinRandom { get; set; }
+        public int MaxRandom { get; set; }
+    }
+
+    [TestClass]
+    public class UnitTestForSparseTableTest
+    {
+        private readonly Settings _settings = new Settings
+        {
+            ShowAll = false,
+            OnlyFailed = true,
+            OnlyPassed = false,
+            IterationCount = 100,
+            SubIterationCount = 100,
+            MinRandom = -100,
+            MaxRandom = 100
+        };
+
+        private int _totalTestCase;
+        private int _passed;
+        private int _falied;
+
+        [TestMethod]
+        public void Test()
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(Environment.NewLine);
+            sb.Append(Environment.NewLine);
+
+            Random random = new Random(9997);
+
+            for (int k = 0; k < _settings.IterationCount; ++k)
+            {
+                int n = random.Next(1, 1000);
+                int[] array = new int[n];
+
+                for (int j = 0; j < n; j++)
+                {
+                    var next = random.Next(_settings.MinRandom, _settings.MaxRandom);
+                    array[j] = next;
+                }
+
+                var table = SparseTable.BuildSparseTable(array);
+
+                for (int i = 0; i < _settings.SubIterationCount; i++)
+                {
+                    try
+                    {
+                        ++_totalTestCase;
+
+                        var ka = random.Next(0, n);
+                        var kb = random.Next(0, n);
+
+                        var a = Math.Min(ka, kb);
+                        var b = Math.Max(ka, kb);
+
+                        //TEST CASE RUN
+                        var got = SparseTable.Solve(a, b, table);
+                        int expected = SparseTable.SolveBf(array, a, b);
+
+                        try
+                        {
+                            Assert.AreEqual(expected, got);
+                            ++_passed;
+
+                            if (_settings.ShowAll || _settings.OnlyPassed)
+                            {
+                                sb.Append(Environment.NewLine);
+                                sb.Append($"NEW TEST CASE: N = {n} ,A = {a},B = {b}");
+                                sb.Append(Environment.NewLine);
+                                sb.Append("MESSAGE");
+                                sb.Append(Environment.NewLine);
+
+                                foreach (var wval in array)
+                                {
+                                    sb.Append(wval + " ");
+                                }
+
+                                sb.Append(Environment.NewLine);
+                                sb.Append(Environment.NewLine);
+
+                                sb.Append($"got : {got} vs expected: {expected}");
+                                sb.Append($" --> PASSED TEST CASE");
+                                sb.Append(Environment.NewLine);
+                            }
+
+                        }
+                        catch (Exception e)
+                        {
+                            ++_falied;
+
+                            sb.Append(Environment.NewLine);
+                            sb.Append($"NEW TEST CASE: N = {n} ,A = {a},B = {b}");
+                            sb.Append(Environment.NewLine);
+                            sb.Append("MESSAGE");
+                            sb.Append(Environment.NewLine);
+
+                            foreach (var wval in array)
+                            {
+                                sb.Append(wval + " ");
+                            }
+
+                            sb.Append(Environment.NewLine);
+                            sb.Append(Environment.NewLine);
+
+                            sb.Append($"got : {got} vs expected: {expected}");
+                            sb.Append($" --> Failed TEST CASE");
+                            sb.Append(Environment.NewLine);
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        ++_falied;
+
+                        sb.Append(Environment.NewLine);
+                        sb.Append($"NEW TEST CASE: N = {n}");
+                        sb.Append(Environment.NewLine);
+                        sb.Append("MESSAGE");
+                        sb.Append(Environment.NewLine);
+
+                        foreach (var wval in array)
+                        {
+                            sb.Append(wval + " ");
+                        }
+
+                        sb.Append(Environment.NewLine);
+
+                        sb.Append($" --> Exception in TEST CASE : Exception : {exception.Message},  {exception.StackTrace}");
+                        sb.Append(Environment.NewLine);
+                    }
+                }
+            }
+
+            sb.Insert(0, $"Total Test cases run = {_totalTestCase}, Passed = {_passed}, Failed = {_falied}");
+            File.WriteAllText("unit-test-input.txt", sb.ToString());
+
+        }
+    }
+}
